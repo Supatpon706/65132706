f0bfe03490870c035ae565f3ec2303227e315316
Supatpon706 <supatpontreetong@gmail.com>
2024-06-21 11:47:36 +0700

    Create 65132706-2.dart

---
+++ 65132706-2.dart
@@ -0,0 +1,14 @@
+void main() {
+  // กำหนดตัวแปร
+  int number = 14;
+  // ตรวจสอบเลขคู่หรือเลขคี่
+  if (number % 2 == 0) {
+    print("$number is even.");
+  } else {
+    print("$number is odd.");
+  }
+    // พิมพ์ตัวเลขจาก 1 ถึง 5
+  for (int i = 1; i <= 5; i++) {
+    print("Number: $i");
+  }
+}
